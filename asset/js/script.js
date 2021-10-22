ee5e4bf14599f494e9353406d899bbdb3cc3c234
syaiful505 <syaifulstyle505@gmail.com>
2021-10-23 01:34:23 +0700

    mengatur menu home

--- asset/js/script.js
+++ asset/js/script.js
@@ -18,3 +18,21 @@ function linkAction(){
     navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/* Scroll active di salah satu section */
+const section = document.querySelectorAll('section[id]')
+window.addEventListener('scroll', scrollActive)
+function scrollActive(){
+    const scrollY = window.pageYOffset
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+        
+        if(scrollY > sectionTop && scrollY <= sectionTop +sectionHeight){
+            document.querySelectorAll('.nav__menu a[href*='+ sectionId +']').classList.add('active')
+        }else{
+            document.querySelectorAll('.nav__menu a[href*='+ sectionId +']').classList.remove('active')
+        }
+    })
+}
