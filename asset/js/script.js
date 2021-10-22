b86f63663a67d6ea79e557836737f350de9632d6
syaiful505 <syaifulstyle505@gmail.com>
2021-10-23 01:03:03 +0700

    Menghilangkan menu setelah di klik

--- asset/js/script.js
+++ asset/js/script.js
@@ -12,4 +12,9 @@ closeMenu.addEventListener('click', ()=>{
     navMenu.classList.remove('show')
 })
 
-/* Menghilangkan Menu */
+/* Menghilangkan Menu ketika setelah di klik */
+const navLink = document.querySelectorAll('.nav__link')
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
